4aa91e489938f7b2fbe359442c62a3631fc66d8b
Abhinav Bajpai <Abhinav123bajpai@gmail.com>
2025-08-21 10:42:09 +0530

    complete

--- index.js
+++ index.js
@@ -1,12 +1,63 @@
-const getTodosSync = () => {};
+const fs = require('fs');
 
-const getTodoSync = (id) => {};
+const getTodosSync = () => {
+  const text = fs.readFileSync('db.txt', 'utf-8');
+  return text;
+};
 
-const createTodoSync = (todo) => {};
+const getTodoSync = (id) => {
+  const text = fs.readFileSync('db.txt', 'utf-8');
+  const arr = JSON.parse(`[${text.split("}\n{").join("},{")}]`);
 
-const updateTodoSync = (id, updates) => {};
+  const obj = arr.find(d => d.id===id);
+  return JSON.stringify(obj);
+};
 
-const deleteTodoSync = (id) => {};
+const createTodoSync = (todo) => {
+  const obj = {
+    title: todo,
+    isCompleted: false,
+    createdAt: new Date().toISOString(),
+    updatedAt: new Date().toISOString(),
+  }
+  const str = JSON.stringify(obj, null, 2);
+  fs.writeFileSync('db.txt', str);
+};
+
+const updateTodoSync = (id, updates) => {
+  const text = fs.readFileSync('db.txt', 'utf-8');
+  const arr = JSON.parse(`[${text.split("}\n{").join("},{")}]`);
+
+  for(let i in arr) {
+    let obj = arr[i];
+    if(obj.id===id) {
+      obj = {
+        ...obj,
+        ...updates,
+        updatedAt: new Date().toISOString(),
+      }
+      arr[i] = obj;
+    }
+  }
+  let str="";
+  for(let obj of arr) {
+    str+=JSON.stringify(obj)+"\n";
+  }
+  fs.writeFileSync('db.txt', JSON.stringify(str, null, 2));
+};
+
+const deleteTodoSync = (id) => {
+  const text = fs.readFileSync('db.txt', 'utf-8');
+  const arr = JSON.parse(`[${text.split("}\n{").join("},{")}]`);
+
+  let str="";
+  for(let obj of arr) {
+    if(obj.id!==id) {
+      str+=JSON.stringify(obj)+"\n";
+    }
+  }
+  fs.writeFileSync('db.txt', JSON.stringify(str, null, 2));
+};
 
 module.exports = {
   getTodosSync,
